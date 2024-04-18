e280dc9fb83c3f083ac5fe83e7d8f6fbaadb184a
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2024-04-18 16:12:32 -0700

    Made the base for the project - Divij

---
+++ Realtime-Stock-App/src/main/java/realtime_stock_app/MainApplication.java
@@ -0,0 +1,16 @@
+package main.java.realtime_stock_app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // full model here
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
