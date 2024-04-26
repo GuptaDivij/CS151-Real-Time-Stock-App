bbc0e0f1ccd037b799bbacd1901b96e5f7518b7f
GuptaDivij <131484950+GuptaDivij@users.noreply.github.com>
2024-04-25 23:11:05 -0700

    Portfolio fixed, handling errors

--- RealtimeStockApp/src/main/java/realtime_stock_app/realtime_stock_app/Portfolio.java
+++ RealtimeStockApp/src/main/java/realtime_stock_app/realtime_stock_app/Portfolio.java
@@ -1,5 +1,4 @@
 package realtime_stock_app.realtime_stock_app;
-import java.io.IOException;
 import java.util.*;
 public class Portfolio {
     private HashMap<Stock, Integer> portfolioList;
@@ -13,16 +12,19 @@ public class Portfolio {
     }
 
     public void sellStocks(Stock stock, int quantity) throws Exception {
-        if(quantity > getPortfolio().get(stock)){
+        if(!portfolioList.containsKey(stock)){
+            throw new Exception("Stock not found in the portfolio");
+        }
+        if(quantity > portfolioList.get(stock)) {
             throw new Exception("Cannot sell more quantity than owned in the portfolio");
         }
-        getPortfolio().put(stock, getPortfolio().get(stock)-quantity);
+        portfolioList.put(stock, portfolioList.get(stock)-quantity);
     }
 
     public double calculateNetWorth(){
         double networth = 0;
-        for(Stock stock: getPortfolio().keySet()){
-            networth += stock.getCurrentPrice() * getPortfolio().get(stock);
+        for(Stock stock: portfolioList.keySet()){
+            networth += stock.getCurrentPrice() * portfolioList.get(stock);
         }
         return networth;
     }
