2147babfc0c3a927aa55e4d25216286193630c0f
Ved Joshi <50056400+Ved-Joshi@users.noreply.github.com>
2024-05-04 01:26:12 -0700

    Update Stock Portfolio UI and add controller

---
+++ RealtimeStockApp/src/main/java/realtime_stock_app/realtime_stock_app/StockPortfolioController.java
@@ -0,0 +1,9 @@
+package realtime_stock_app.realtime_stock_app;
+
+import javafx.scene.input.MouseEvent;
+
+public class StockPortfolioController {
+
+    public void handleStockSearch(MouseEvent mouseEvent) {
+    }
+}
