58dd64ccf422663a23cbbf528bc0e589b31c28c0
Ved Joshi <50056400+Ved-Joshi@users.noreply.github.com>
2024-05-03 18:06:25 -0700

    add handling for login

--- RealtimeStockApp/src/main/java/realtime_stock_app/realtime_stock_app/StockLoginController.java
+++ RealtimeStockApp/src/main/java/realtime_stock_app/realtime_stock_app/StockLoginController.java
@@ -1,9 +1,11 @@
 package realtime_stock_app.realtime_stock_app;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -13,6 +15,10 @@ public class StockLoginController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
 
     public void handleWelcomeOpen(MouseEvent mouseEvent) throws IOException {
         FXMLLoader welcomeLoader = new FXMLLoader(getClass().getResource("StockWelcome.fxml"));
@@ -21,5 +27,21 @@ public class StockLoginController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+
+    public void handleLogin(MouseEvent mouseEvent) throws IOException {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        // Check if username and password match
+        boolean loggedIn = false;
+
+            FXMLLoader portfolioLoader = new FXMLLoader(getClass().getResource("StockPortfolio.fxml"));
+            root = portfolioLoader.load();
+            stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
     }
 }
