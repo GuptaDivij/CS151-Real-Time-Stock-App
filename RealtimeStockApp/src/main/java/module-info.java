0e2c9c4d96973359017b7c20c61dc4e99341cfb4
Nish Oberoi <mustangno7@gmail.com>
2024-05-03 18:34:46 -0700

    JSON file

--- RealtimeStockApp/src/main/java/module-info.java
+++ RealtimeStockApp/src/main/java/module-info.java
@@ -6,6 +6,8 @@ module realtime_stock_app.realtime_stock_app {
     requires org.kordamp.bootstrapfx.core;
     requires java.net.http;
     requires mongo.java.driver;
+    requires json;
+    requires json.simple;
 
     opens realtime_stock_app.realtime_stock_app to javafx.fxml;
     exports realtime_stock_app.realtime_stock_app;
