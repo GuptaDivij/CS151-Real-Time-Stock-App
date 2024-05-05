715eb9181f028b3e7b45c1323f528bebb27f250c
Nish Oberoi <mustangno7@gmail.com>
2024-05-04 23:26:10 -0700

    Portfolio Controller

--- RealtimeStockApp/src/main/java/realtime_stock_app/realtime_stock_app/StockPortfolioController.java
+++ RealtimeStockApp/src/main/java/realtime_stock_app/realtime_stock_app/StockPortfolioController.java
@@ -6,32 +6,40 @@ import javafx.scene.input.MouseEvent;
 import java.io.IOException;
 
 public class StockPortfolioController {
+
+    @FXML
+    private TextField stockPane;
+    @FXML
+    private TextField noResults;
     @FXML
     private TextField searchBar;
     public void handleStockSearch(MouseEvent mouseEvent) throws IOException, InterruptedException {
         String stockTicker = searchBar.getText();
-        try {
-            Stock stock = new Stock(stockTicker);
 
-            // Display stock information
-            stockSymbolLabel.setText("Symbol: " + stock.getSymbol());
-            currentPriceLabel.setText("Current Price: $" + stock.getCurrentPrice());
-            priceChangeLabel.setText("Price Change: " + String.format("%.2f%%", stock.getPriceChange() * 100));
 
-            // Enable the button to add to portfolio
-            addToPortfolioButton.setDisable(false);
-        } catch (Exception e) {
-            // If stock doesn't exist, display an error message
-            stockSymbolLabel.setText("Error: Stock not found");
-            currentPriceLabel.setText("");
-            priceChangeLabel.setText("");
-            addToPortfolioButton.setDisable(true);
-        }
-
-        //if stock exists do this else throw an error message
+        //if stock exists make stockPane visible and no reults invisible
 
 
         //display latest stock price and price change and give option to "+" (add) to portfolio (right side of the scene)
+
+        boolean stockExists = stockExists(stockTicker);
+
+        if(stockExists){
+            noResults.setVisible(false);
+            stockPane.setVisible(true);
+            displayStockDetails(stockTicker);
+        }
+        else{
+            stockPane.setVisible(false);
+            noResults.setVisible(true);
+        }
+    }
+
+    private void displayStockDetails(String stockTicker) {
+
+    }
+
+    private boolean stockExists(String stockTicker) {
 
     }
 
