538267b5cf7d07057853c6866dcb1d4219b2febf
Nish Oberoi <mustangno7@gmail.com>
2024-05-04 22:19:40 -0700

    Search

--- RealtimeStockApp/src/main/java/realtime_stock_app/realtime_stock_app/StockPortfolioController.java
+++ RealtimeStockApp/src/main/java/realtime_stock_app/realtime_stock_app/StockPortfolioController.java
@@ -10,10 +10,26 @@ public class StockPortfolioController {
     private TextField searchBar;
     public void handleStockSearch(MouseEvent mouseEvent) throws IOException, InterruptedException {
         String stockTicker = searchBar.getText();
+        try {
+            Stock stock = new Stock(stockTicker);
+
+            // Display stock information
+            stockSymbolLabel.setText("Symbol: " + stock.getSymbol());
+            currentPriceLabel.setText("Current Price: $" + stock.getCurrentPrice());
+            priceChangeLabel.setText("Price Change: " + String.format("%.2f%%", stock.getPriceChange() * 100));
+
+            // Enable the button to add to portfolio
+            addToPortfolioButton.setDisable(false);
+        } catch (Exception e) {
+            // If stock doesn't exist, display an error message
+            stockSymbolLabel.setText("Error: Stock not found");
+            currentPriceLabel.setText("");
+            priceChangeLabel.setText("");
+            addToPortfolioButton.setDisable(true);
+        }
 
         //if stock exists do this else throw an error message
 
-        Stock stk = new Stock(stockTicker);
 
         //display latest stock price and price change and give option to "+" (add) to portfolio (right side of the scene)
 
