79387a80dbc55f6d8c2aa4e5a059e3ee687f3f46
Ved Joshi <50056400+Ved-Joshi@users.noreply.github.com>
2024-05-06 22:23:34 -0700

    Add Stock Sell

---
+++ RealtimeStockApp/src/main/java/realtime_stock_app/realtime_stock_app/StockSellController.java
@@ -0,0 +1,8 @@
+package realtime_stock_app.realtime_stock_app;
+
+import javafx.scene.input.MouseEvent;
+
+public class StockSellController {
+    public void handleSellStocks(MouseEvent mouseEvent) {
+    }
+}
